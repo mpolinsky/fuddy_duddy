3736e2a205eccfc6b7cda2df66e11d034f6580e2
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-19 09:44:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -40,14 +40,14 @@ if st.session_state.count > 5:
 	
     st.session_state.res.append('red')
 with st.form(key=str(dt.now())):	
-	option = st.selectbox(
-	    'Select:',
-	    st.session_state.word_pool)
-	submit = st.form_submit_button("submit")
-	if submit:
-	    st.write('You selected:', option)
-	    st.session_state.choice = option	
-	    st.session_state.res.append(st.session_state.choice)
+    option = st.selectbox(
+        'Select:',
+        st.session_state.word_pool)
+    submit = st.form_submit_button("submit")
+    if submit:
+        st.write('You selected:', option)
+	st.session_state.choice = option	
+	st.session_state.res.append(st.session_state.choice)
 
 st.write(st.session_state)
 
