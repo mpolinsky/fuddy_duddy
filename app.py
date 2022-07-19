bfff374891c1cd5ebd5a9bfc6e9ca7e3c4e80562
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-19 11:39:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -48,12 +48,15 @@ if 'count' not in st.session_state:
 if 'name' not in st.session_state or st.session_state.name == "":
     st.session_state.name = st.text_input("Enter name")
 	
-if 'counter1' not in st.session_state:
-    st.session_state.counter1 = Co(st.session_state.name)
+if 'counter' not in st.session_state:
+    st.session_state.counter = ""
 
 
 st.write('Count = ', st.session_state.count)
 
+if st.session_state.count == 2:
+    st.session_state.counter = Co(st.session_state.name)
+	
 
 option = st.selectbox(
 'Select:',
