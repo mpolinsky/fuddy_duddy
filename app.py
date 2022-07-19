ed4e30c1a1f0100d5dad1dca1b3864bcf39e3812
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-19 11:53:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ if 'count' not in st.session_state:
 if 'name' not in st.session_state or st.session_state.name == "":
     st.session_state.name = st.text_input("Enter name")
 	
-if 'counter' not in st.session_state:
+if 'counter1' not in st.session_state:
     st.session_state.counter1 = Co(st.session_state.name)
 
 st.write('Count = ', st.session_state.count)
