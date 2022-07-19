87501ba987592095d0f6ed3a11ec9d49f98ce0b6
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-19 08:01:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ if 'res' not in st.session_state:
 # If count is already initialized, don't do anything
 if 'count' not in st.session_state:
 	st.session_state.count = 0
+	st.write("ONE TIME")
 
 # Create a button which will increment the counter
 increment = st.button('Increment')
