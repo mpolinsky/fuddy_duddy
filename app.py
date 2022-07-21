d11bf5111d1edbb6fa53dafd624726340792d609
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-20 21:09:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import english_words as ew
 from datetime import datetime as dt
 from collections import Counter as Co
 
-
-optio = st.selectbox("Select:", options=[1,2,3,4,5])
-st.subheader(optio)
+with st.form(key=str(dt.now())):
+  optio = st.selectbox("Select:", options=[1,2,3,4,5])
+  st.subheader(optio)
+  submit = st.form_submit_button("Submit")
+  if submit:
+    st.header("HGELOOOO")
