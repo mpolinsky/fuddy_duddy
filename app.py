7cd5d6a7a52f1548218dadcccc46b540b10e7c30
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-20 21:12:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ with st.form(key=str(dt.now())):
   optio = st.selectbox("Select:", options=[1,2,3,4,5])
   st.subheader(optio)
   submit = st.form_submit_button("Submit")
-  if submit:
-    st.header("HGELOOOO")
-    st.session_state.hello = 99
+if submit:
+  st.header("HGELOOOO")
+  st.session_state.hello = 99
     
 st.header(st.session_state.hello)
