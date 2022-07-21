fbe75776cefb67b8685bcc5240d0e9de001b1be2
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-20 21:31:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ def assign(arg):
 if 'hello' not in st.session_state:
   st.session_state.hello = 55555
   
-with st.form(key=str(dt.now())):
-  selection = st.selectbox("Select:", options=[1,2,3,4,5])
-  st.subheader(selection)
-  submit = st.form_submit_button("Submit", on_click=assign, args=(selection,))
-  if submit:
-    st.header("HELLO WORLD")
-    st.session_state.hello = 99999
+#with st.form(key=str(dt.now())):
+st.session_state.hello = st.selectbox("Select:", options=[1,2,3,4,5])
+  #st.subheader(selection)
+  #submit = st.form_submit_button("Submit", on_click=assign, args=(selection,))
+  #if submit:
+    #st.header("HELLO WORLD")
+    #st.session_state.hello = 99999
     
 st.header(st.session_state.hello)
 
