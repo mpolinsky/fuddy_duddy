0359e626647e906d25575e4cd3a5b998925d8dca
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-24 19:11:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import english_words as ew
 
 
 
-st.header("This is the [link you want.] (https://www.npr.org)")
-st.subheader("This is the [link you want.] (https://www.npr.org)")
-st.write("This is the [link you want.] (https://www.npr.org)")
+#st.header("This is the [link you want.] (https://www.npr.org)")
+#st.subheader("This is the [link you want.] (https://www.npr.org)")
+st.write("This is the [link](https://www.npr.org)")
 
 
 st.session_state
