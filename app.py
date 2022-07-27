00f826c013029c8f3cdf67d1575d805c98426360
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-27 10:58:18 -0400

    Reverting to the test multiselect box
    Really would like to make it so that the menu can be reselected

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
 import streamlit as st
+from datetime import datetime as dt
+import english_words as ew
 
-with st.expander("Hi Charly"):
-    if st.checkbox("Fake expand"):
-        st.write("Hello world")
+def assign(arg):
+  st.session_state.hello = arg
+
+if 'word_pool' not in st.session_state:
+    st.session_state.word_pool = [i for i in ew.english_words_lower_alpha_set if len(i) > 3][:200]
+  
+if 'hello' not in st.session_state:
+  st.session_state.hello = 55555
+  
+  
+if 'accumulator' not in st.session_state:
+  st.session_state.accumulator = list()
+  
+#with st.form(key=str(dt.now())):
+st.session_state.hello = st.selectbox("Select:", options=st.session_state.word_pool)
+  #st.subheader(selection)
+  #submit = st.form_submit_button("Submit", on_click=assign, args=(selection,))
+  #if submit:
+    #st.header("HELLO WORLD")
+    #st.session_state.hello = 99999
+st.session_state.accumulator.append(st.session_state.hello)
+    
+st.header(st.session_state.hello)
+
+st.session_state.word_pool = st.session_state.word_pool[:-10]
+
 
 st.session_state
