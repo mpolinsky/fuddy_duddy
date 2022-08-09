08c54776e95991054c3d95dfed1dbfd8e5b26e71
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-08-09 15:37:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import streamlit.components.v1 as components
 
 
 #if st.button(label = "Tweet"):
-components.html("""<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>""")
+components.html("""<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="&quot;TEST WORLD&quot;" data-url="https://www.cnn.com" data-dnt="true" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>""")
 
 	
