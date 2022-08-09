db10572fb1bf2e37e4549f5224de6e3b1831134b
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-08-09 15:11:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.button(components.html("""<a class='twitter-share-button'
+st.button(label = "Tweet", components.html("""<a class='twitter-share-button'
 			href='https://twitter.com/intent/tweet'
 			data-text="Wello Hurld"
 			data-url='https://streamlit.io'
