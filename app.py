b10816a98ef473518f50f09eb536173e5bdf1413
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-08-09 15:02:24 -0400

    Changing to twitter widget demo
    Can't get this to work in Anagrams so I'm trying it out here.  It may be about spacing and syntax...this was copied from the Streamlit forums from January 22

--- app.py
+++ app.py
@@ -1,38 +1,14 @@
 import streamlit as st
-from datetime import datetime as dt
-import english_words as ew
-
-def assign(arg):
-  st.session_state.hello = arg
-
-if 'word_pool' not in st.session_state:
-    st.session_state.word_pool = [i for i in ew.english_words_lower_alpha_set if len(i) > 3][:200]
-  
-if 'hello' not in st.session_state:
-  st.session_state.hello = 55555
-  st.session_state.count = 0
-  
-if 'accumulator' not in st.session_state:
-  st.session_state.accumulator = list()
-  
-
-st.session_state.word_pool.insert(0,None) 
-with st.form(key=str(dt.now())):
-  selection= st.selectbox("Select:", options=st.session_state.word_pool)
-  st.subheader(selection)
-  submit = st.form_submit_button("Submit", on_click=assign, args=(selection,))
-  if submit:
-    st.header("HELLO WORLD")
-    st.session_state.hello = 99999
-if selection is not None:
-  st.session_state.hello = selection
-  st.session_state.accumulator.append(st.session_state.hello)
-st.session_state.count += 1
-    
-st.header(st.session_state.hello)
-st.button("submit")
-  
-st.session_state.word_pool = st.session_state.word_pool[:-10]
+import streamlit.components.v1 as components
 
 
-st.session_state
+components.html("""<a class='twitter-share-button'
+				href='https://twitter.com/intent/tweet'
+				data-text=st.session_state.anagram
+				data-url='https://streamlit.io'
+				data-show-count='false'>
+				data-size='Large'
+				data-hashtags='streamlit,python'
+				</a>
+				<script src='https://platform.twitter.com/widgets.js' charset='utf-8'></script>
+				""")
